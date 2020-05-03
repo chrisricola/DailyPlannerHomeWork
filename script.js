df4252ba6ad81e3e746a5e8834cea721b940763e
Chris Rico <chris.r.rico@gmail.com>
2020-05-02 19:43:16 -0700

    updates

--- script.js
+++ script.js
@@ -4,23 +4,16 @@ console.log(currentTime);
 $("#currentDay").text(moment().format("MMM Do YY"));
 
 $(".saveBtn").click(function() {
-    var txtValue = $(this).siblings(".txtArea#").val();
-    var txtKey = $(this).siblings(".txtArea#").attr("id")
-    console.log(txtKey);
+    var txtValue = $(this).prev(".txtArea").val();
+    var txtKey = $(this).prev(".txtArea").attr("id")
     localStorage.setItem(txtKey,txtValue)
-})
+    console.log(txtKey,txtValue);
+});
 
 // render local storage
-$('.txtArea9').val(localStorage.getItem('9'))
-$('.txtArea10').val(localStorage.getItem('10'))
-$('.txtArea11').val(localStorage.getItem('11'))
-// $('.txtArea12').val(localStorage.getItem('12'));
-// $('.txtArea13').val(localStorage.getItem('13'))
-// $('.txtArea14').val(localStorage.getItem('14'))
-// $('.txtArea15').val(localStorage.getItem('15'))
-// $('.txtArea16').val(localStorage.getItem('16'))
-// $('.txtArea17').val(localStorage.getItem('17'))
-
+$('.txtArea').val(localStorage.getItem('9'))
+$('.txtArea').val(localStorage.getItem('10'))
+// $('.txtArea').val(localStorage.getItem('11'))
 
 
 //set the format of timeblocks to dynamically change its color based on time of the day
